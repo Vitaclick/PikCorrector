fd6c17b7248c34c4a974abdddd3598caeb0966d4
Vitaclick <vitaliy.zema@gmail.com>
2018-12-17 11:17:10 +0300

    elegancy #01

--- Program.cs
+++ Program.cs
@@ -7,34 +7,55 @@ using System.Text;
 using System.Xml.Linq;
 
 namespace PikCorrector
-{    class Program
+{
+  class Program
   {
+
     static void Main(string[] args)
     {
       var userPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
       var pluginsConfigpath = Path.Combine(userPath, @"Autodesk\Revit\Addins\2017\PIK\PIK_PluginConfig.xml");
 
+      var pluginsToRemove = new List<string>
+      {
+        "ChangeManager",
+        "RevitNameValidator",
+        "OkCommand",
+        "Revit_RebarColorFiller"
+      };
+
+      void removePlugin(XDocument doc, string keyValue)
+      {
+        doc.Descendants("PluginInfo")
+          .Where(x => x.Element("AssemblyName").Value == keyValue)
+          .Remove();
+      }
+
+      if (args.Length > 0)
+      {
+
+        if (args[0] == "1")
+        {
+          pluginsToRemove.AddRange(new List<string> {
+          "FamilyManager",
+          "FamilyExplorer",
+          "BimInspector.Revit",
+          "InspectorConfig"
+        });
+        }
+      }
+
+
       XDocument xmlDoc = XDocument.Load(pluginsConfigpath);
-      xmlDoc.Descendants("PluginInfo")
-      .Where(x => x.Element("AssemblyName").Value == "ChangeManager")
-      .Remove();
-      xmlDoc.Descendants("PluginInfo")
-      .Where(x => x.Element("AssemblyName").Value == "RevitNameValidator")
-      .Remove();
-      xmlDoc.Descendants("PluginInfo")
-      .Where(x => x.Element("AssemblyName").Value == "Revit_RebarColorFiller")
-      .Remove();
-      xmlDoc.Descendants("PluginInfo")
-      .Where(x => x.Element("AssemblyName").Value == "OkCommand")
-      .Remove();
-      xmlDoc.Descendants("PluginInfo")
-      .Where(x => x.Element("AssemblyName").Value == "BimInspector.Revit")
-      .Remove();
-      xmlDoc.Descendants("PluginInfo")
-      .Where(x => x.Element("AssemblyName").Value == "InspectorConfig")
-      .Remove();
+
+      foreach (var r in pluginsToRemove)
+      {
+        removePlugin(xmlDoc, r);
+      }
 
       xmlDoc.Save(pluginsConfigpath);
+
+
 
       // delete unnesesery folders
       var deleters = new List<string> {
@@ -48,14 +69,17 @@ namespace PikCorrector
       //   deleters.Add("vitro path...");
       // }
 
-      foreach(var path in deleters) {
-      if (Directory.Exists(path))
-        try {
-        Directory.Delete(path, true);
-        }
-        catch (Exception ex){
-          Debug.Write(ex.Message);
-        }
+      foreach (var path in deleters)
+      {
+        if (Directory.Exists(path))
+          try
+          {
+            Directory.Delete(path, true);
+          }
+          catch (Exception ex)
+          {
+            Debug.Write(ex.Message);
+          }
       }
     }
   }
